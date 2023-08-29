8bedcac1e6fee05b2de69a041086027115e850b0
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-08-30 00:09:31 +0200

    Segundo commit

---
+++ hola-mundo/js/05-tipodedatos.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//operadores
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("El resultado de operacion es: " + operacion);
+
+//Tipo de datos
+
+var numero_entero = 44;
+var cadena_texto = "Hola q tal";
+var verdadero_o_falso = true;
+
+//funciones
+var numero_falso = "33.4";
+
+console.log(String(numero_falso)+7);
