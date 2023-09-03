4f6f14b137c166b1edd02c986384a148505e23fb
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-04 00:10:01 +0200

    ejercicio parametros rest y spread

---
+++ hola-mundo/js/23-parametros2_45.js
@@ -0,0 +1,15 @@
+'use estrict';
+
+//parametros REST y SPREAD
+
+function listadoFrutas(fruta1,fruta2, ...resto_de_frutas){
+    console.log("Fruta 1: "+fruta1);
+    console.log("Fruta 2: "+fruta2);
+    console.log(resto_de_frutas);
+}
+
+listadoFrutas("Naranja", "Manzana", "Peras", "Sandias","Coco","platano");
+
+var frutas = ["Naranja", "Manzana"];
+
+listadoFrutas(...frutas,"Peras", "Sandias","Coco","platano");
