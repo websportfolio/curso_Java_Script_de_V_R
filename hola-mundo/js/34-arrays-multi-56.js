7d1af06d6251e971f4c531d9e2f430db1eb2f275
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-08 16:55:36 +0200

    Ejercicio 56 Arrays multidimencionales

---
+++ hola-mundo/js/34-arrays-multi-56.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Arrays multidimencionales
+
+var categorias =  ['Accion','terror','Comedia'];
+var peliculas = ['La verdad duele','La vida es bella','Gran Torino'];
+var cine = [categorias, peliculas];
+console.log(cine[1][2]);
