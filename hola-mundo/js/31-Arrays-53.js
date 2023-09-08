870f25a4e767d84b2ce6dbf615bf836829d2e081
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-08 14:46:56 +0200

    Ejercicio 53 Arrays basicos

---
+++ hola-mundo/js/31-Arrays-53.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Arrays, Arreglos, Matrizes basicos
+
+var nombres = ["Ruslan Komarytskiy","Victor Robles","Juan Perez","Ignacio Gonzales", 52, true];
+var lenguajes = new Array("PHP", "JS", "Go","Java");
+
+//console.log(nombres);
+//console.log(lenguajes);
+
+console.log(nombres[0]);
