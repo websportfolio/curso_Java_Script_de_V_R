dcbf96b570b8e29869bf7933c1d452dda5f9d40e
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-09 15:04:44 +0200

    Ejercicio 59 Ordenar un array

---
+++ hola-mundo/js/37-ordenarArray-59.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Ordenar array
+
+var categorias =  ['Accion','terror','Comedia'];
+var peliculas = ['La verdad duele','La vida es bella','Gran Torino'];
+
+console.log(peliculas.sort());//ordena alfabeticamente
+console.log(peliculas.reverse());//invierte array
+
+
+var cine = [categorias, peliculas];
