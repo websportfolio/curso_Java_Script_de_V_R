3def2f85634be34639e4bc7366943bf2096ad884
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-09 14:52:44 +0200

    Ejercicio 59 convertir un string en un array

---
+++ hola-mundo/js/36-convStrEnArray-58.js
@@ -0,0 +1,8 @@
+'use estrict';
+
+//convertir una cadena en un array
+
+var cadena = "texto1, texto2, texto3";
+var cadena_array = cadena.split(", ");//convierte una cadena en array separada por coma y espacio
+
+console.log(cadena_array);
