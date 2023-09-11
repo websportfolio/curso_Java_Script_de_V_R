cbc355ea95f2b046c2766e89410a0a8a353acce1
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-11 02:00:28 +0200

    Leccion 69 Eventos de raton

---
+++ hola-mundo/js/45-Eventos-69.js
@@ -0,0 +1,38 @@
+'use strict';
+
+//Eventos del raton
+
+
+
+function cambiarColor(){
+    console.log("Me has dado un click")
+    var bg =  boton.style.background;
+    if(bg == "green"){
+        boton.style.background = "red";
+      
+    }else{
+        boton.style.background="green";
+    }
+
+    boton.style.padding = "10px";
+    boton.style.border = "1px solid #ccc";
+
+return true;
+   
+}
+
+var boton = document.querySelector("#boton");
+//Evento click 
+boton.addEventListener('click', function(){
+    cambiarColor();
+});
+
+//Mouseover
+
+boton.addEventListener('mouseover',function(){
+    boton.style.background = "yellow";
+})
+//mouseout
+boton.addEventListener('mouseout',function(){
+    boton.style.background = "#ccc";
+})
