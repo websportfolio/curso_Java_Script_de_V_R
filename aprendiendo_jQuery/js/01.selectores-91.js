b9b54a758c4400bd75cd02a8684b71fbca10dca6
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-15 03:56:03 +0200

    Leccion 91. Selector ID

---
+++ aprendiendo_jQuery/js/01.selectores-91.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    console.log("Cargado...")
+
+    //Selector de id
+
+    var rojo = $('#rojo').css('background',"red")
+                        .css('color','white');
+    var amarillo = $('#amarillo').css('background',"yellow")
+                                .css('color', 'green');  
+                                
+                $("#verde").css("background","green")
+                            .css("color","white")                
+    
+
+
+});
