a465334493a8d77c2f9c07156c3359544fadd773
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-17 21:22:22 +0200

    Leccion 04. Selector de atributos

---
+++ aprendiendo_jQuery/js/04-selector -de-atributo-94.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //Selector de atributos
+    $('[title = "Google"]').css('background','#ccc');   
+    $('[title = "Facebook"]').css('background', 'blue');
+    $('[title = "Facebook"]').css('color', 'white' ); 
+});
+
