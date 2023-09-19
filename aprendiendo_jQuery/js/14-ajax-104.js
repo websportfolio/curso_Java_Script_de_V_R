c96b5538d7c63bbb96811e970c2006109ae1aa27
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-19 06:08:14 +0200

    Leccion 106. Metodo Get de Ajax

--- aprendiendo_jQuery/js/14-ajax-104.js
+++ aprendiendo_jQuery/js/14-ajax-104.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $("#datos").load('https://reqres.in/')
+   // $("#datos").load('https://reqres.in/')
+
+    //Leccion 106 peticiones de ajax metodo Get
+    $.get('https://reqres.in/api/users',{page: 2}, function(response){
+            response.data.forEach((element, index )=> {
+                $("#datos").append("<p>"+element.first_name+" "+element.last_name+"</p>");
+            });
+    })
 })
