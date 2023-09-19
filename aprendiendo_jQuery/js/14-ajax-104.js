c471d925f2613f37d6b24ebad530101b606758e8
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-19 14:00:19 +0200

    Leccion 107. Metodo POST de Ajax

--- aprendiendo_jQuery/js/14-ajax-104.js
+++ aprendiendo_jQuery/js/14-ajax-104.js
@@ -6,5 +6,23 @@ $(document).ready(function(){
             response.data.forEach((element, index )=> {
                 $("#datos").append("<p>"+element.first_name+" "+element.last_name+"</p>");
             });
+    });
+    //Leccion 107. metodo Post de Ajax
+   
+
+    $('#formulario').submit(function(e){
+        e.preventDefault();//para que no rediriga la pagina
+        var usuario = {
+            name: $('input[name="name"]').val(),
+            web: $('input[name="web"]').val()
+        };
+        console.log(usuario);
+        $.post($(this).attr("action"), usuario, function(response){
+    console.log(response)
+        }).done(function(){
+            alert("Usuario añadido correctamente");
+        })
+        return false;
     })
+
 })
