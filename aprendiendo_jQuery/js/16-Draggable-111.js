186db81f0376d8d4a84d2a620fe4a92e9eccb7c1
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-09-20 04:43:26 +0200

    Leccion 112. Resizable redimenciona elemento

--- aprendiendo_jQuery/js/16-Draggable-111.js
+++ aprendiendo_jQuery/js/16-Draggable-111.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     console.log("Hola draggable")
     //Leccion 111. Draggable --> Mover elemento por la pagina
     $(".elemento").draggable();
+
+    //Leccion 112. Resizable--> Redimencionar elemento
+    $('.elemento').resizable();
 })
