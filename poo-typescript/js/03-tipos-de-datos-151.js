ce1e6c8fa65d048e462f08069b8c96e584d96d23
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-12 22:14:23 +0200

    Leccion 151. Variables y typos de datos en TS

---
+++ poo-typescript/js/03-tipos-de-datos-151.js
@@ -0,0 +1,13 @@
+//string
+var cadena = "victorrobles.es";
+//number
+var numero = 12;
+//Booleano
+var verdadero_falso = true;
+// any
+var cualquiera = "Hola";
+cualquiera = 77;
+// Arrays
+var lenguajes = ["PHP", "JS", "CSS", 12];
+var years = [12, 76, 34];
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
---
+++ poo-typescript/js/03-tipos-de-datos-151.ts
@@ -0,0 +1,20 @@
+
+//string
+let cadena: string = "victorrobles.es";
+
+//number
+let numero: number = 12;
+
+//Booleano
+let verdadero_falso: boolean = true;
+
+// any
+let cualquiera: any = "Hola";
+cualquiera = 77;
+
+// Arrays
+var lenguajes: Array<any> = ["PHP","JS","CSS",12];
+
+let years:number[] = [12,76,34];
+
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
