f34040debd4784d48ca28180560c766af7e7876f
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-13 05:49:48 +0200

    Leccion 155. Funciones

---
+++ poo-typescript/js/04-funciones-155.js
@@ -0,0 +1,7 @@
+//Leccion 155
+//tipo de datos es numero y devuelve string
+function getNumero(numero) {
+    if (numero === void 0) { numero = 12; }
+    return "El numero es: " + numero;
+}
+console.log(getNumero(55));
---
+++ poo-typescript/js/04-funciones-155.ts
@@ -0,0 +1,7 @@
+//Leccion 155
+//tipo de datos es numero y devuelve string
+function getNumero(numero:number = 12):string{
+    return "El numero es: "+numero;
+}
+
+console.log(getNumero(55));
