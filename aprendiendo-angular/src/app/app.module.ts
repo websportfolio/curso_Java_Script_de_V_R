094769aeef0708ddb55c06340bd9d1dc8db948c5
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-25 06:05:54 +0200

    Leccion Leccion 170. Crear Componentes angular

---
+++ aprendiendo-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VideojuegoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
