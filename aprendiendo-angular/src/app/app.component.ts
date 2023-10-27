0fcf62314b77ee73c40add44c49e17353ce73ba9
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-27 06:29:11 +0200

    Leccion 175. Import y export

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Configuracion } from './models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'Master de Java Script y Angular!';
   public mostrar_videjuegos: boolean = true;
+  public descripcion :string;
+  public config;
+  constructor(){
+    this.config = Configuracion;
+    this.title = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+  }
 
   ocultarVideojuegos(value: boolean){
     this.mostrar_videjuegos = value;
---
+++ aprendiendo-angular/src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export var Configuracion = {
+    color: "red",
+    fondo: "#eee",
+    titulo: "Master en Java script y Angular",
+    descripcion: "Aprendiendo Angular con Victor Robles - victorrobles.es"
+};
