37d782a8a4d30f6c43ed0e8eaffe8b3b929888bd
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-31 18:44:30 +0100

    Leccion 179. Directiva bucle ngFor

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -8,12 +8,15 @@ import { Zapatilla } from '../models/zapatilla';
 
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas!";
-    public zapatillas: Array<Zapatilla>
+    public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
     constructor(){
+      this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Nike Air', 'Nike','Red',40, true),
             new Zapatilla('Reebok Classic', 'Reebok','Blanco',80, true),
+            new Zapatilla('Reebok Spartan', 'Reebok','Black',180, true),
             new Zapatilla('Nice Runner', 'Nike','Black',60, true),
             new Zapatilla('Adidas Yizzy', 'Adidas','Grey',130, false)
         ];
@@ -21,5 +24,24 @@ export class ZapatillasComponent implements OnInit{
 
     ngOnInit(): void {
       console.log(this.zapatillas);
+
+      this.getMarcas();
     }
+
+    getMarcas(){
+      this.zapatillas.forEach((zapatilla, index) =>{
+
+        //para ver q no se duplican
+        //en el caso q no lo encuentre
+        if(this.marcas.indexOf(zapatilla.marca)<0){
+          //cuando no existe en array hago push
+          this.marcas.push(zapatilla.marca);
+        }
+
+
+          //console.log(index);
+      });
+      console.log(this.marcas);
+    }
+
 }
