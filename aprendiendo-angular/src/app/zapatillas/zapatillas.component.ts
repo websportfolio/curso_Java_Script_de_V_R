537d984c0a7300ff70f86aa2c6bfb2ec643a4afc
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-10-31 10:58:14 +0100

    Leccion 178. Directiva bucle ngFor

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -12,7 +12,7 @@ export class ZapatillasComponent implements OnInit{
 
     constructor(){
         this.zapatillas = [
-            new Zapatilla('Nike Air', 'Nike','Red',190, true),
+            new Zapatilla('Nike Air', 'Nike','Red',40, true),
             new Zapatilla('Reebok Classic', 'Reebok','Blanco',80, true),
             new Zapatilla('Nice Runner', 'Nike','Black',60, true),
             new Zapatilla('Adidas Yizzy', 'Adidas','Grey',130, false)
