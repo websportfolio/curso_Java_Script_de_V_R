9574ab271d3f0619f0df6220ce493c1ac5076a4b
RuslanKomarytskiy <kruslan55569@gmail.com>
2023-11-09 11:26:12 +0100

    Leccion 190. Parametros por url

--- aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit{
+  public nombre: any;
+  public followers: number = 0;
+
+
+  ngOnInit(): void {
+    // Your initialization code goes here
+    this._route.params.subscribe((params: Params) =>{
+      this.nombre = params['nombre'];
+      this.followers = +params['followers'];
+      console.log(typeof +params['followers']);
+    });
+  }
+
+  constructor(
+
+    private _route: ActivatedRoute,
+    private _router: Router,
+
+
+
+  ){}
+
 
 }
